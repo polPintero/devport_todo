eb8f8cd1a9f62f376a75758d507b20a12f0611ea
Dmitriy <johndoe@example.com>
2023-04-02 21:40:26 +0300

    created basic inputnumber

---
+++ src/components/InputNumber.vue
@@ -0,0 +1,20 @@
+<template>
+  <InputComp :label="label" />
+</template>
+
+<script>
+import InputComp from './InputComp.vue'
+
+export default {
+  name: 'InputNumber',
+  components: { InputComp },
+  props: {
+    label: {
+        type: String,
+        default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
