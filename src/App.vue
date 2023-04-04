a8a8ec2431904883117be12d15c171b769ce7ab2
Dmitriy <johndoe@example.com>
2023-04-04 10:03:33 +0300

    created login form

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,25 @@ export default {
 html {
   background: #212121;
   color: #ffff;
+  font-size: 18px;
+  font-family: Montserrat,sans-serif;
+}
+
+body {
+  min-height: 100vw;
+  min-height: 100vh;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+#app {
+  .login-form {
+    box-shadow: 1.5px 1.5px 3px #0e0e0e, -1.5px -1.5px 3px rgb(95 94 94 / 25%), inset 0px 0px 0px #0e0e0e, inset 0px 0px 0px #5f5e5e;
+    border-radius: 5px;
+  }
+
 }
 </style>
--- src/components/organism/LoginForm.vue
+++ src/components/organism/LoginForm.vue
@@ -1,18 +1,41 @@
 <template>
-  <InputComp label="username" />
-  <InputNumber label="phone" />
+  <form class="login-form" @submit.prevent>
+    <h4 class="login-form__title">Login</h4>
+    <InputComp label="username" />
+    <InputNumber label="phone" />
+    <ButtonComp label="login" />
+  </form>
 </template>
 
 <script>
 import InputComp from '@/components/atoms/InputComp.vue'
 import InputNumber from '@/components/atoms/InputNumber.vue'
+import ButtonComp from '@/components/atoms/ButtonComp.vue'
+
 export default {
   name: 'LoginForm',
   components: {
     InputComp,
-    InputNumber
+    InputNumber,
+    ButtonComp
   }
 }
 </script>
 
-<style></style>
+<style lang="scss">
+.login-form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  padding: 20px;
+
+  &__title {
+    text-align: center;
+    font-weight: 700;
+    font-size: 2em;
+    margin: 0;
+  }
+}
+</style>
