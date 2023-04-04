cea46a051a5b4dbd1e43fe965cc879176ce0f675
Dmitriy <johndoe@example.com>
2023-04-04 18:36:28 +0300

    created user card

--- src/components/organism/UserCard.vue
+++ src/components/organism/UserCard.vue
@@ -1,13 +1,16 @@
 <template>
-    <section class="user-card">
-        <h5 class="user-card__name">{{ user.name }}</h5>
-        <span class="user-card__address">{{ getAdress() }}</span>
-        <section class="user-card__contacts">
-            <span v-for="(value, key) in createContacts()" class="user-card__contacts--elem" :key="key">
-                <img :src="icons[key]">
-            </span>
+    <div class="user-card--wrap">
+        <section class="user-card">
+            <h5 class="user-card__name">{{ user.name }}</h5>
+            <span class="user-card__address">{{ getAdress() }}</span>
+            <span class="user-card__company">{{ user.company.name }}</span>
+            <section class="user-card__contacts">
+                <span v-for="(value, key) in createContacts()" class="user-card__contacts--elem" :key="key" :title="value">
+                    <img :src="icons[key]">
+                </span>
+            </section>
         </section>
-    </section>
+    </div>
 </template>
 
 <script>
@@ -51,20 +54,55 @@ export default {
 </script>
 
 <style lang="scss">
+.user-card--wrap {
+    background-image: linear-gradient(163deg, #00ff75 0%, #3700ff 100%);
+    margin: 30px auto;
+    border-radius: 10px;
+    transition: all .2s;
+    box-shadow: 1.5px 1.5px 3px #0e0e0e, -1.5px -1.5px 3px rgb(95 94 94 / 25%), inset 0px 0px 0px #0e0e0e, inset 0px 0px 0px #5f5e5e;
+
+    &:hover {
+        box-shadow: 0px 0px 30px 1px rgba(0, 255, 117, 0.30);
+
+        .user-card {
+            transform: scale(0.98);
+        }
+    }
+}
+
 .user-card {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding: 30px;
+    border-radius: 10px;
+    transition: all .2s;
+    background: #212121;
+    text-align: center;
+
+    &__name {
+        margin-block-start: 0;
+        margin-block-end: 10px;
+        font-size: 1.5rem;
+    }
 
     &__address {
         font-size: 0.8rem;
-
+        font-style: italic;
     }
 
     &__contacts {
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+
         &--elem {
+            cursor: pointer;
+
             img {
                 width: 15px;
                 color: white
             }
         }
     }
-}
-</style>
+}</style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,5 +1,6 @@
 <template>
-    <LoginForm v-model:userName="userName" v-model:phoneNumber="phoneNumber" @autorizaition="autorizaition" />
+    <LoginForm class="login-page" v-model:userName="userName" v-model:phoneNumber="phoneNumber"
+        @autorizaition="autorizaition" />
 </template>
 
 <script>
@@ -11,24 +12,32 @@ export default {
     },
     data () {
         return {
-            userName: 'Leanne Graham',
+            userName: 'Bret',
             phoneNumber: '1-770-736-8031 x56442'
         }
     },
     methods: {
         async autorizaition () {
             const response = await this.$store.dispatch('getUsers')
-            const isUser = this.checkUser(response)
-            if (isUser) {
+            const user = this.findUser(response)
+            if (user) {
+                this.$store.commit('setUser', user)
                 this.$router.push(this.$router.routeList.todo)
             }
-
         },
-        checkUser (arr) {
-            return !!arr.find(i => i.name === this.userName && i.phone === this.phoneNumber)
+        findUser (arr) {
+            return arr.find((i) => i.username === this.userName && i.phone === this.phoneNumber)
         }
     }
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.login-page {
+    margin: auto;
+    margin-block-start: 50vh;
+    transform: translateY(-50%);
+    box-shadow: 1.5px 1.5px 3px #0e0e0e, -1.5px -1.5px 3px rgb(95 94 94 / 25%), inset 0px 0px 0px #0e0e0e, inset 0px 0px 0px #5f5e5e;
+    border-radius: 5px;
+}
+</style>
