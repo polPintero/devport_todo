29830129369767071f0687b4d13bfe2e8f9ebfee
Dmitriy <johndoe@example.com>
2023-04-04 10:03:19 +0300

    created btn

---
+++ src/components/atoms/ButtonComp.vue
@@ -0,0 +1,37 @@
+<template>
+  <button class="btn-comp">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonComp',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.btn-comp {
+  background-color: transparent;
+  color: inherit;
+  padding: 10px 20px;
+  font-size: 1rem;
+  border-radius: 5px;
+  text-transform: capitalize;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  transition: box-shadow ease 0.1s;
+  box-shadow: 1.5px 1.5px 3px #0e0e0e, -1.5px -1.5px 3px rgb(95 94 94 / 25%),
+    inset 0px 0px 0px #0e0e0e, inset 0px -0px 0px #5f5e5e;
+
+  &:active {
+    box-shadow: 0px 0px 0px #0e0e0e, 0px 0px 0px rgb(95 94 94 / 25%), inset 3px 3px 4px #0e0e0e,
+      inset -3px -3px 4px #5f5e5e;
+  }
+}
+</style>
