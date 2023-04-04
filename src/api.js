eed690e476c3d78cdd167e1f9c82d8e75aba2de5
Dmitriy <johndoe@example.com>
2023-04-04 11:21:05 +0300

    created API class

---
+++ src/api.js
@@ -0,0 +1,26 @@
+class API {
+  constructor(domain) {
+    this.domain = domain
+  }
+  async sendRequest (url = '/', params = {}) {
+    let response = await fetch(url, params)
+    try {
+      response = await response.json()
+    } catch (err){
+      console.log(err)
+    }
+    return response
+  }
+}
+
+class DevPortAPI extends API {
+  async getUsers () {
+    let url = this.domain + '/users'
+    const response = await this.sendRequest(url)
+    return response
+  }
+}
+
+export default new DevPortAPI('https://jsonplaceholder.typicode.com/')
+
+
