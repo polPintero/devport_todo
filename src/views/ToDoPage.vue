eb5a01a5a85fe4363c0f6ef787f31ac7a78b3fe1
Dmitriy <johndoe@example.com>
2023-04-04 18:42:38 +0300

    basic creted todolist

--- src/views/ToDoPage.vue
+++ src/views/ToDoPage.vue
@@ -1,16 +1,25 @@
 <template>
-    <UserCard />    
+    <div class="todo-page">
+        <UserCard />
+        <ToDoList />
+    </div> 
 </template>
 
 <script>
 import UserCard from '@/components/organism/UserCard.vue'
+import ToDoList from '@/components/organism/ToDoList.vue'
 
 export default {
     name: 'ToDoPage',
     components:{
-        UserCard
+        UserCard,
+        ToDoList
     },
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.todo-page {
+    width: 100%;
+}
+</style>
